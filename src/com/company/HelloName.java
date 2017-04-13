2e1a19022b704cbe0fda02c1d77d5a870e9ded57
RiditaNizam <riditan@gmail.com>
2017-04-13 00:33:00 -0400

    Initial commit

---
+++ src/com/company/HelloName.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by ridita on 4/12/17.
+ */
+public class HelloName {
+
+    public String helloName(String name) {
+        return ("Hello" + " " + name + "!");
+    }
+
+}
